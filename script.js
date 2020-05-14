c4468c8495ce4918208e4d6a11fb110e49ae6064
Sergej <shuklin.sergey@gmail.com>
2020-05-14 16:51:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
+let url = 'http://51-zendev.gateway.lightwing.io/zenwork-login';
 // setInterval(()=>{window.open('http://51-zendev.gateway.lightwing.io/zenwork-login');}, 1800000);
 
-setInterval(()=>{window.open('http://51-zendev.gateway.lightwing.io/zenwork-login');}, 1000);
+setInterval(()=>{console.log(url); window.open(url);}, 1000);
 
